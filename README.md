a052cef20bc9a9a156cd952b47d506e05f6bdd18
ze.su <ze.su@qunar.com>
2017-03-15 14:44:06 +0800

    一些基本的配置以及简单demo

--- README.md
+++ README.md
@@ -1,2 +1,8 @@
 # es6Demo
 es6Demo for share
+
+#### 本地服务器
+webpack-dev-server
+
+#### 实时编译
+webpack --server
--- index.js
+++ index.js
@@ -1,1 +1,6 @@
-// init
+let a = [1, 2, 3];
+let b = function ({a, b}) {
+    document.write(a + b);
+    // console.log(a + b);
+}
+b({a: 3, b: 3});
